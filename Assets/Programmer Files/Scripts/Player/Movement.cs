5f386edd788a04fe62c5b1761fa0a55717b0c863
TeaSerpent <digitalShadow2523@gmail.com>
2024-11-11 18:34:01 +0000

    Testing?
    Testing? Script.

---
+++ Assets/Programmer Files/Scripts/Player/Movement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float speed = 700f;
+
+    private Rigidbody2D UnknownPlayersBody;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
