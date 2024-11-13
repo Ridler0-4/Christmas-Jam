100ca29c98560250a3618f8d4b207796def6a350
TeaSerpent <digitalShadow2523@gmail.com>
2024-11-13 21:32:36 +0000

    Collision Detection Done
    Collision detection done

--- Assets/Programmer Files/Scripts/Environment/Collision detection.cs
+++ Assets/Programmer Files/Scripts/Environment/Collision detection.cs
@@ -3,18 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class Collisiondetection : MonoBehaviour
 {
+    bool Detect = false;
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
+
         if (collider2D.tag == "Player" || collider2D.tag == "Ice Block")
         {
-            Cursor.visible = true;
-            Debug.Log("trap activated");
+            Debug.Log("Detected!");
+            Detect = true;
         }
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        gameObject.GetComponent<Collider>().isTrigger = true;
+        gameObject.GetComponent<Collider2D>().isTrigger = true;
     }
 
     // Update is called once per frame
