9b8bb34d9243f4bde3fff03a0e507b29aadb8fe0
Ridler0-4 <bi77sg@student.sunderland.ac.uk>
2024-11-13 23:55:55 +0000

    added ice block
    ice block

--- Assets/Programmer Files/Scripts/Environment/Collision detection.cs
+++ Assets/Programmer Files/Scripts/Environment/Collision detection.cs
@@ -1,17 +1,33 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Collisiondetection : MonoBehaviour
 {
     bool Detect = false;
+    [SerializeField]
+    int CollisionType = 0;
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
 
-        if (collider2D.tag == "Player" || collider2D.tag == "Ice Block")
+        while (collider2D.tag == "Player" || collider2D.tag == "Ice Block")
         {
             Debug.Log("Detected!");
             Detect = true;
+            if (CollisionType == 0)
+            {
+                //Jump
+            }
+            else if (CollisionType == 1)
+            {
+                //Pressure plate
+            }
+            else if (CollisionType == 2)
+            {
+                //Spike
+            }
         }
+  
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
